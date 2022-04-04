d1320121293c645b97e04c0bd11232224b7b4179
yankovskaya <yankovskaya.my@gmail.com>
2022-04-04 18:47:38 +0300

    Add plan_client/Update schemas, plannigs

--- frontend/views/app.py
+++ frontend/views/app.py
@@ -1,17 +1,24 @@
-from flask import Blueprint, render_template
+import logging
+from flask import Blueprint, redirect, render_template
+from frontend.clients.plan_client import PlanningClient
 
+from datetime import datetime
 from frontend.clients.user_client import UserClient
 from frontend.config import ENDPOINT
 
-
 uapp = Blueprint('user', __name__)
 
+logger = logging.getLogger(__name__)
+
 users_client = UserClient(ENDPOINT)
+plan_client = PlanningClient(ENDPOINT)
 
 
 @uapp.route('/')
 def index():
-    return render_template('index.html')
+    plan = plan_client.add(name='', date=datetime.now())
+    logger.info(plan)
+    return redirect('<plan_id>')
 
 
 @uapp.route('/users')
