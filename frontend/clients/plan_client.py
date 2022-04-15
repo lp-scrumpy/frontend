53e8de65e903e725364f992c54d0bd7840ee7643
Vladimir Puzakov <puzakov@tochka.com>
2022-04-15 03:48:39 +0300

    fix: get plan by id

--- frontend/clients/plan_client.py
+++ frontend/clients/plan_client.py
@@ -1,5 +1,6 @@
 import orjson
 from datetime import datetime
+from typing import Optional
 import httpx
 
 from frontend.schemas import Plan
@@ -9,15 +10,19 @@ class PlanningClient:
     def __init__(self, url: str):
         self.url = f'{url}/plannings/'
 
-    def get_by_id(self, planning_id: int) -> Plan:
+    def get_by_id(self, planning_id: int) -> Optional[Plan]:
         res = httpx.get(f'{self.url}{planning_id}')
+        if res.status_code == 404:
+            return None
+
         res.raise_for_status()
-        plan = Plan(res.json())
-        return plan
+
+        plan = res.json()
+        return Plan(**plan)
 
     def add(self, name: str, date: datetime) -> Plan:
-        plan = Plan(uid=-1, name=name, date=date)
-        payload = orjson.dumps(plan.dict())
+        new_plan = Plan(uid=-1, name=name, date=date)
+        payload = orjson.dumps(new_plan.dict())
         res = httpx.post(
             self.url,
             content=payload,
@@ -25,5 +30,6 @@ class PlanningClient:
             timeout=20,
         )
         res.raise_for_status()
+
         plan = res.json()
         return Plan(**plan)
