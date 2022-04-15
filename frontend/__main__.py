f8b700a26bcc469f9874d3569ac3e1ab593c6ff3
yankovskaya <yankovskaya.my@gmail.com>
2022-04-15 18:28:37 +0300

    update templates

--- frontend/__main__.py
+++ frontend/__main__.py
@@ -14,7 +14,7 @@ def main():
     app.secret_key = config.SECRET_KEY
     app.register_blueprint(view, url_prefix='/')
     app.register_blueprint(plannings.view, url_prefix='/plannings')
-    app.run(host=config.APP_HOST, port=config.APP_PORT)
+    app.run()
 
 
 if __name__ == '__main__':
